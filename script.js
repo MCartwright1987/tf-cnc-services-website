1483dc5db9471c9dc0bd470e2b383cc1eb69c7f7
MCartwright1987 <matthewcartwright1987@yahoo.com>
2024-02-12 19:49:06 +0000

    added parallax effect with java script

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function() {
+    var titleImageAndTxt = document.querySelector('.titleImageAndTxt');
+    var img = document.querySelector('.titleImageAndTxt img');
+  
+    var scrollY = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
+
+    // Adjust the translateY value to control the downward movement
+    var translateY = Math.min(190, scrollY * 0.6); // Adjust the factor as needed
+  
+    img.style.transform = 'translate(50px, ' + translateY + 'px)';
+  });
