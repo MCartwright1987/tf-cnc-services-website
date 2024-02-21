42f6629be83ee4e387a0808ba2009683f497d6bc
MCartwright1987 <matthewcartwright1987@yahoo.com>
2024-02-21 20:42:42 +0000

    refactored css

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.addEventListener('scroll', function() {
-    var titleImageAndTxt = document.querySelector('.heroContainer');
+    var heroContainer = document.querySelector('.heroContainer');
     var img = document.querySelector('.heroContainer img');
   
     var scrollY = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
 
     // Adjust the translateY value to control the downward movement
-    var translateY = Math.min(190, scrollY * 0.6); // Adjust the factor as needed
+    var translateY = Math.min(220, scrollY * 0.6);
   
     img.style.transform = 'translate(0px, ' + translateY + 'px)';
   });
