ed6b8b6bae65c0fd72dc6330b7ce347e71954b48
MCartwright1987 <matthewcartwright1987@yahoo.com>
2024-02-22 21:51:18 +0000

    cleaned up home page design

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', function() {
     var scrollY = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
 
     // Adjust the translateY value to control the downward movement
-    var translateY = Math.min(220, scrollY * 0.6);
+    var translateY = Math.min(190, scrollY * 0.6);
   
     img.style.transform = 'translate(0px, ' + translateY + 'px)';
   });
